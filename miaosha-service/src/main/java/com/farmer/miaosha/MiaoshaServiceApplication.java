0f83f93e3ed100f33e302fe1c2cf5c4081ae9b4e
Willinggang <willinggang@gmail.com>
2020-01-10 17:08:31 +0800

    添加注册中心

--- miaosha-service/src/main/java/com/farmer/miaosha/MiaoshaServiceApplication.java
+++ miaosha-service/src/main/java/com/farmer/miaosha/MiaoshaServiceApplication.java
@@ -2,8 +2,10 @@ package com.farmer.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MiaoshaServiceApplication {
 
     public static void main(String[] args) {
