4d6f29e71d658bbf260eb695547cf9095b00d9ad
Willinggang <willinggang@gmail.com>
2020-05-20 17:03:28 +0800

    添加订单服务

--- order-service/src/main/java/com/farmer/order/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/farmer/order/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.farmer.product;
+package com.farmer.order.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class ProductServiceApplication {
+public class OrderServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(OrderServiceApplication.class, args);
     }
 
 }
---
+++ product-service/src/main/java/com/farmer/product/remote/ProductRemoteService.java
@@ -0,0 +1,10 @@
+package com.farmer.product.remote;
+
+/**
+ * @program: FocusingJava
+ * @description: 商品远程接口调用服务
+ * @author: FarmerSun
+ * @create: 2020-05-20 16:41
+ */
+public interface ProductRemoteService {
+}
